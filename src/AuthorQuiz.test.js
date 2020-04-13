1a2ad8ae98dd08dea150801f02e8bc3bbc0ee65b
DinaTaklit <ez_taklit@esi.dz>
2020-04-13 14:54:32 +0100

    feat: test if the application render without cruch

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './AuthorQuiz';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
+import AuthorQuiz from './AuthorQuiz';
+// Add imports fro test 
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+const state = {
+  turnData: {
+    books: ['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities', 'Hamlet', 'Macbeth', 'Romeo and Juliet'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+  },
+  highlight: 'none'
+}
+
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />, div);
+  });
+
 });
