8abc060d689e306b7e4e8fc1a739d20398d5ce07
DinaTaklit <ez_taklit@esi.dz>
2020-04-14 18:31:29 +0100

    feat: Link AddAuthor form in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, withRouter} from 'react-router-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import * as serviceWorker from './serviceWorker';
 import {shuffle, sample} from 'underscore';
 // add author list 
@@ -85,12 +86,21 @@ function App(){
       render();
     }}/>);
 }
+const AuthorWrapper = withRouter(({history}) =>
+  <AddAuthorForm onAddAuthor={(author) => {
+    authors.push(author);
+    history.push('/');
+  }}>
+
+  </AddAuthorForm>
+);
 
 function render(){
   ReactDOM.render(
     <BrowserRouter>
     <React.Fragment>
       <Route exact path="/" component={App}></Route>
+      <Route path="/add" component={AuthorWrapper}></Route>
     </React.Fragment>
     </BrowserRouter>,
     document.getElementById('root')
