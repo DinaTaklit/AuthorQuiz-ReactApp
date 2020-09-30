38d263a2d86520912cb4622d3ea9f0cb5c2d167a
DinaTaklit <ez_taklit@esi.dz>
2020-09-30 23:28:02 +0100

    Updates

---
+++ precache-manifest.b438c207f0cdad8cb60cc4416401baae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9bfd434171b74de7ecbdedc67daf7c",
+    "url": "/AuthorQuiz-ReactApp/index.html"
+  },
+  {
+    "revision": "af470f94de880e91dd61",
+    "url": "/AuthorQuiz-ReactApp/static/css/main.be9a4c6d.chunk.css"
+  },
+  {
+    "revision": "0726870d2007ed79c491",
+    "url": "/AuthorQuiz-ReactApp/static/js/2.02676fb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AuthorQuiz-ReactApp/static/js/2.02676fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af470f94de880e91dd61",
+    "url": "/AuthorQuiz-ReactApp/static/js/main.24d05e64.chunk.js"
+  },
+  {
+    "revision": "bb0a041e70d447970920",
+    "url": "/AuthorQuiz-ReactApp/static/js/runtime-main.078faa86.js"
+  }
+]);
